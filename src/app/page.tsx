bc122c291f230fbbdd509666fa7a23f7a6f42f46
bhaveshlande <bhavesh.baliram-lande@capgemini.com>
2023-12-05 12:41:47 +0530

    project basic setup and header added

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
+
+export default function Home() {
+  return (
+    <div className="div grid gris-cols-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
+}
